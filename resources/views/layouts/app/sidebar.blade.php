dbfd306c0a8358724c04bd80d6123fccdcd8cdaa
blockpc <soporte.blockpc@gmail.com>
2026-07-19 14:10:05 -0400

    try to fix some details

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -6,7 +6,7 @@
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky collapsible="mobile" class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.header>
-                <flux:link :href="route('dashboard')" wire:navigate>
+                <flux:link variant="subtle" :href="route('dashboard')" wire:navigate>
                     <x-new-logo />
                 </flux:link>
                 <flux:sidebar.collapse class="lg:hidden" />
